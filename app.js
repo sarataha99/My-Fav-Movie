97e84d1975430d06995e46c1042eed0e7247085c
sarataha22 <>
2021-04-16 12:25:58 +0000

    hello

---
+++ app.js
@@ -0,0 +1,16 @@
+
+var userAge=prompt('please Enter Your Age ');
+if (userAge >= 18){
+  alert('welcome can you see my Website');
+}else{
+  alert( 'you are baby');
+}
+console.log(userAge);
+if (userAge >= 18){
+
+document.write('<img src= "https://miro.medium.com/max/1838/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg">')}
+else{
+
+document.write('<img src=" https://images.unsplash.com/photo-1566004100631-35d015d6a491?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"')
+}
+
