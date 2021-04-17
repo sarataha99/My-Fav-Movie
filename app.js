a803da72295c5cd8ac308e5ac496979f84b22be7
sarataha22 <>
2021-04-17 19:13:07 +0000

    hello

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var userAge=prompt('please Enter Your Age ');
 if (userAge >= 18){
   alert('welcome can you see my Website');
 }else{
-  alert( 'you are baby');
+  alert( 'you are a baby');
 }
 console.log(userAge);
 if (userAge >= 18){
@@ -13,4 +13,6 @@ else{
 
 document.write('<img src=" https://images.unsplash.com/photo-1566004100631-35d015d6a491?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"')
 }
+
+
 
---
+++ lab04.js
@@ -0,0 +1,21 @@
+
+/*var age = prompt('Hello to my website ,please Enter Your Age);
+
+if (age >=18 && age<=26){
+  document.write('welcome to my website ');
+} else {
+
+  document.write('you can not see my websit ');
+
+}else{
+  alert('please enter your age as a number');
+*/
+var actorName=prompt('Enter Your fav actor Name ');
+
+if(actorName=='Tom cruise'){
+document.write('<img src=https://cdn.al-ain.com/images/2020/9/23/176-151455-tom-cruise-find-out-when-new-mission-impossible_700x400.jpg')}
+else{actorName=='will smitt'
+document.write('img src=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw3ZmhKkuqh8QGdSE8PrOVciMdLLdrnEP7_g&usqp=CAU')
+
+
+}
